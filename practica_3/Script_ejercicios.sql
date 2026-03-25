d06e3e947a3a35f7a5340b9c0b83839332242895
Rodrigo G. García <garciarod1408@gmail.com>
2026-03-25 12:17:11 -0600

    join con 3 tablas

--- practica_3/Script_ejercicios.sql
+++ practica_3/Script_ejercicios.sql
@@ -12,5 +12,12 @@ inner JOIN t_productos prod
 ON prod.id_proveedor = prov.id_proveedor
 where prov.id_proveedor = 45636; /* obtener id 45636 haciendo un inner join*/
 
+select * from t_ventas
 
-
+SELECT vent.id_ventas, cli.nombre, cli.apellido,prod.modelo, prod.marca, vent.fecha 
+from t_ventas vent
+inner JOIN t_clientes cli
+ON vent.id_clientes = cli.id_clientes
+inner JOIN t_productos prod
+ON vent.id_producto = prod.id_productos
+WHERE fecha = '2015-02-06'   /* join con 3 tablas*/
